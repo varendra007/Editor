6ca51c6ec68fade85cff0f65905817a8adab025d
Hrishikesh Pable <87420795+Hrishikesh2002@users.noreply.github.com>
2022-10-14 15:06:41 +0530

    add event.js
    adds an event using EventEmmiter function

---
+++ src/event.js
@@ -0,0 +1,13 @@
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
