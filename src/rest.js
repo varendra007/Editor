c7e0f885863e09e2d0be683da2f432cec423fb4e
Hrishikesh Pable <87420795+Hrishikesh2002@users.noreply.github.com>
2022-10-14 15:11:55 +0530

    add rest.js
    connects to the rest API

---
+++ src/rest.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/listUsers', function (req, res) {
+   fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+      console.log( data );
+      res.end( data );
+   });
+})
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
