13ccc2ab30e08600bce023e38aa09d5c8103d732
Udayansh <udayansh02092001@gmail.com>
2022-10-19 18:33:40 +0530

    added file read data

--- src/utils/fileRead.js
+++ src/utils/fileRead.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.readFile('TestFile.txt', function (err, data) {
+fs.readFile('FileReadData.txt', function (err, data) {
                     if (err) throw err;
 
     console.log(data);
