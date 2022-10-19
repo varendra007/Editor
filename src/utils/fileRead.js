1eb234b71047d7468436cc167722e9b80ac7c7ce
Udayansh <udayansh02092001@gmail.com>
2022-10-19 18:40:29 +0530

    corrected string in fileRead

--- src/utils/fileRead.js
+++ src/utils/fileRead.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-fs.readFile('FileReadData.txt', function (err, data) {
+fs.readFile('FileRead.txt', function (err, data) {
                     if (err) throw err;
 
     console.log(data);
