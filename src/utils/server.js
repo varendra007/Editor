0b081c5d7a28339a316aec72c250986003c5a889
Varendra Maurya <88757164+varendra007@users.noreply.github.com>
2022-10-21 00:34:31 +0530

    Update server.js

--- src/utils/server.js
+++ src/utils/server.js
@@ -1,18 +1,12 @@
-var http = require('http'); // Import Node.js core module
+var http = require('http'); 
 
-var server = http.createServer(function (req, res) {   //create web server
-    if (req.url == '/') { //check the URL of the current request
-        
-        // set response header
+var server = http.createServer(function (req, res) {   
+    if (req.url == '/') { 
         res.writeHead(200, { 'Content-Type': 'text/html' }); 
-        
-        // set response content    
         res.write('<html><body><p>This is home Page.</p></body></html>');
         res.end();
-    
     }
     else if (req.url == "/student") {
-        
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.write('<html><body><p>This is student Page.</p></body></html>');
         res.end();
@@ -30,6 +24,6 @@ var server = http.createServer(function (req, res) {   //create web server
 
 });
 
-server.listen(5000); //6 - listen for any incoming requests
+server.listen(5000);
 
 console.log('Node.js web server at port 5000 is running..')
